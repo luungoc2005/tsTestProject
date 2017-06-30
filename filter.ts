be94479782b84ab1e107f1871f55034a34cebee4
Unknown <luungoc2005@gmail.com>
2017-06-30 17:25:39 +0700

    add file filter functionality

---
+++ filter.ts
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const excludedPatterns: RegExp[] = [
+    /^\/api\/biztalkconnector\//i,
+    /^\/api\/authentication\//i,    
+    /^\/api\/authorization\//i,  
+    /^\/api\/workflow\//i,
+];
+
+(function () {
+    const inputFile: string = fs.readFileSync('./output.json', "utf8");
+    const input: string[] = JSON.parse(inputFile);
+
+    let retVal = [];
+
+    retVal = input.filter(item => {
+        for (let i = 0; i < excludedPatterns.length; i++) {
+            if (excludedPatterns[i].exec(item) != null) return false;
+        }
+        return true;
+    });
+
+    fs.writeFileSync('./output.json', JSON.stringify(retVal, null, 2));
+})();
